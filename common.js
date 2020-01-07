1331d4844e647d4ffc7f750fc28abc4a93c0c185
gracefulwolf <gracefulwolf@naver.com>
2020-01-07 10:53:50 +0900

    aaaa

---
+++ common.js
@@ -0,0 +1,56 @@
+if($.isFunction("checkCommonJs")){
+	checkCommonJs("common.js");		
+}
+
+$(function(){ 
+    srUI.chgList('.ui-chg-list');
+});
+
+var srUI = {
+    checkObj : function(obj){
+		return $(obj).length == 0 ? false : true;
+    },	
+
+    chgList : function(obj){
+        var $wrap = $(obj);
+
+        // 처음 세팅
+        srUI.chgListSet($wrap);
+
+        // 행삭제
+        $('.btn-remove').off('click').on('click', function(){
+            var $thisWrap = $(this).parents('.ui-chg-list');
+
+            if($thisWrap.find('input:checkbox').is(':checked')){
+                $thisWrap.find('input:checked').parents('tr').remove();
+                
+                srUI.chgListSet($thisWrap);
+            };
+        });
+
+        // 행추가
+        $('.btn-add').off('click').on('click', function(){
+            var $thisWrap = $(this).parents('.ui-chg-list');
+
+            $thisWrap.find('table tbody').append('<tr><td><input type="checkbox"></td><td><input type="text"></td><td></td><td><a href="#none">링크</a></td></tr>');
+
+            srUI.chgListSet($thisWrap);
+        });
+
+    },
+
+    chgListSet : function(obj){
+        var $wrap = obj,
+            wrapLen = $wrap.length;
+
+        for(var i = 0; i < wrapLen; i++){
+            var tblListLen = $wrap.eq(i).find('table tbody tr').length;
+
+            for(var l = 0; l < tblListLen; l++){
+                $wrap.eq(i).find('table tbody tr').eq(l).data('list','tblList'+l).attr('list','tblList'+l);
+            };
+        };
+    }
+
+    
+}
